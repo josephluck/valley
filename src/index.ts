16f6af4755dcdd07be1aa2791853958ec63faad8
Joseph Luck <joseph.luck@sky.com>
2020-04-10 19:20:29 +0100

    Support async constraints

--- src/index.ts
+++ src/index.ts
@@ -19,29 +19,90 @@ type IConstraints<Fields extends IFields> = {
     | Array<Constraint<Key, Fields[Key], Fields>>;
 };
 
+type AsyncFieldValidationResult = Promise<string> | never;
+
+type AsyncConstraint<
+  Key extends FieldKey,
+  Value extends FieldValue,
+  Fields extends IFields
+> = (key: Key, value: Value, fields: Fields) => AsyncFieldValidationResult;
+
+type IAsyncConstraints<Fields extends IFields> = {
+  [Key in keyof Fields]:
+    | AsyncConstraint<Key, Fields[Key], Fields>
+    | Array<AsyncConstraint<Key, Fields[Key], Fields>>;
+};
+
 type ValidationResult<Fields> = {
   [Key in keyof Fields]: FieldValidationResult;
 };
 
-export const validate = <
+export function validate<
+  Fields extends IFields,
+  Constraints extends IConstraints<Fields>
+>(fields: Fields, constraints: Constraints): ValidationResult<Fields>;
+
+export function validate<
+  Fields extends IFields,
+  Constraints extends IAsyncConstraints<Fields>
+>(fields: Fields, constraints: Constraints): Promise<ValidationResult<Fields>>;
+
+export function validate<
   Fields extends IFields,
   Constraints extends IConstraints<Fields>
 >(
   fields: Fields,
   constraints: Constraints
-): ValidationResult<Fields> => {
-  return Object.keys(constraints).reduce((acc, key: keyof Fields) => {
+): ValidationResult<Fields> | Promise<ValidationResult<Fields>> {
+  const results = Object.keys(constraints).reduce((acc, key: keyof Fields) => {
     const constraint = constraints[key];
     const value = fields[key];
     const constraintsToRun = (typeof constraint === "function"
       ? [constraint]
       : constraint) as Constraint<any, any, any>[];
+
     return {
       ...acc,
-      [key]: constraintsToRun.reduce(
-        (err, fn) => (err ? err : fn(key, value, fields)),
-        void null
-      ),
+      [key]: constraintsToRun.reduce((err, fn) => {
+        if (anyPromise([err])) {
+          return [...err, fn(key, value, fields)];
+        }
+        if (err) {
+          return err;
+        }
+        return [fn(key, value, fields)];
+      }, []),
     };
   }, fields);
-};
+
+  const isAsync = anyPromise(flatten(Object.values(results)));
+
+  if (isAsync) {
+    const keys = Object.keys(results);
+    const promises = Object.values(results);
+    const doResolves = async () => {
+      let response = {};
+      for (let i = 0, x = promises.length; i < x; i++) {
+        const key = keys[i];
+        const promisesForKey = promises[i];
+        const errs = await Promise.all(promisesForKey);
+        response[key] = errs.find(Boolean);
+      }
+      return response;
+    };
+    return doResolves() as any;
+  } else {
+    return Object.keys(results).reduce((acc, key) => {
+      return {
+        ...acc,
+        [key]: results[key].find(Boolean),
+      };
+    }, results);
+  }
+}
+
+const anyPromise = (values: any[]): boolean =>
+  Boolean(values.find((result) => typeof result !== "string"));
+
+const flatten = <T>(values: T[][]): T[] =>
+  values.reduce((acc, curr) => [...acc, ...curr], []);
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -4,6 +4,16 @@ import { validate } from "../";
 const expectStringMatch = (expected: string) => (_: string, actual: string) =>
   actual === expected ? void null : `Expected ${actual} to be ${expected}`;
 
+const asyncExpectStringMatch = (expected: string) => async (
+  _: string,
+  actual: string
+) =>
+  new Promise<string>((resolve) =>
+    actual === expected
+      ? resolve(void null)
+      : resolve(`Expected ${actual} to be ${expected}`)
+  );
+
 const expectGreaterThan = (expected: number) => (_: string, actual: number) =>
   actual > expected
     ? void null
@@ -51,7 +61,7 @@ test("Validates simple invalid fields", (t) => {
 });
 
 test("Validates multiple constraints per field", (t) => {
-  t.plan(2);
+  t.plan(3);
   type Fields = {
     twenty: number;
     thirty: number;
@@ -65,5 +75,67 @@ test("Validates multiple constraints per field", (t) => {
     thirty: [expectDivisibleBy(10), expectGreaterThan(25)],
   });
   t.equal(typeof result.twenty, "string", "Twenty fails");
+  t.equal(result.twenty, "Expected 20 to be greater than 25");
   t.equal(typeof result.thirty, "undefined", "Thirty passes");
 });
+
+test("Validates with asynchronous constraints", async (t) => {
+  t.plan(2);
+  type Fields = {
+    name: string;
+    age: number;
+  };
+  const fields: Fields = {
+    name: "Sam",
+    age: 35,
+  };
+  const result = await validate(fields, {
+    name: asyncExpectStringMatch("Bob"),
+    age: async () => {
+      try {
+        await awaitResolve();
+      } catch (err) {
+        return "Fails";
+      }
+    },
+  });
+  t.equal(typeof result.name, "string", "Name fails");
+  t.equal(typeof result.age, "undefined", "Age passes");
+});
+
+test("Validates with multiple asynchronous constraints", async (t) => {
+  t.plan(2);
+  type Fields = {
+    age: number;
+  };
+  const fields: Fields = {
+    age: 20,
+  };
+  const result = await validate(fields, {
+    age: [
+      async () => {
+        try {
+          await awaitResolve();
+        } catch (err) {
+          return "First constraint";
+        }
+      },
+      async () => {
+        try {
+          await awaitReject();
+        } catch (err) {
+          return "Second constraint";
+        }
+      },
+    ],
+  });
+  t.equal(typeof result.age, "string", "Age fails");
+  t.equal(
+    result.age,
+    "Second constraint",
+    "Age fails with the correct constraint"
+  );
+});
+
+const awaitResolve = () => new Promise((resolve) => resolve());
+const awaitReject = () => new Promise((_, reject) => reject());
