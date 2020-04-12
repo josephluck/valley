3d4aed8a4b069cdfaf6eb50130e775e2e37ce4c9
Joseph Luck <joseph.luck@sky.com>
2020-04-12 16:52:24 +0100

    Ass support for TaskEither fp constraints

--- src/fp.ts
+++ src/fp.ts
@@ -1,35 +1,87 @@
 import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/pipeable";
 
 export function makeValidator<Fs extends Fields>(
   constraints: FieldConstraintsMap<Fs>
+): (fields: Fs) => E.Either<ValidationResult<Fs>, Fs>;
+
+export function makeValidator<Fs extends Fields>(
+  constraints: AsyncFieldConstraintsMap<Fs>
+): (fields: Fs) => TE.TaskEither<ValidationResult<Fs>, Fs>;
+
+export function makeValidator<Fs extends Fields>(
+  constraints: FieldConstraintsMap<Fs> | AsyncFieldConstraintsMap<Fs>
 ) {
-  return function validate(fields: Fs): E.Either<ValidationResult<Fs>, Fs> {
+  type Return =
+    | E.Either<ValidationResult<Fs>, Fs>
+    | TE.TaskEither<ValidationResult<Fs>, Fs>;
+  return function validate(fields: Fs): Return {
     type Fk = keyof Fs;
     type Fv = Fs[Fk];
 
-    const results = Object.keys(fields).map((fieldKey: Fk) => {
-      const fieldValue = fields[fieldKey];
+    const fieldKeys: Fk[] = Object.keys(fields);
+
+    const results = fieldKeys.reduce((acc, fieldKey: Fk) => {
+      const fieldValue: Fv = fields[fieldKey];
       const fieldConstraint = constraints[fieldKey];
       const fieldConstraints = (typeof fieldConstraint === "function"
         ? [fieldConstraint]
         : fieldConstraint) as ConstraintFn<any, any, any>[];
 
-      return fieldConstraints.reduce(
-        (acc, fn) => (E.isLeft(acc) ? acc : fn(fieldValue, fieldKey, fields)),
-        E.right<string, Fv>(fieldValue)
-      );
-    });
+      return {
+        ...acc,
+        [fieldKey]: fieldConstraints.reduce(
+          (err, fn) => [...err, fn(fieldValue, fieldKey, fields)],
+          []
+        ),
+      };
+    }, fields);
 
-    const hasFailed = results.some(E.isLeft);
+    const resultsArr = Object.values(results);
+    const constraintsAreAsync = anyTaskEither(flatten(resultsArr));
+
+    if (constraintsAreAsync) {
+      return pipe(
+        TE.tryCatch(
+          async () => {
+            let asyncResults: ValidationResult<Fs> = {} as any;
+            for (let i = 0, x = resultsArr.length; i < x; i++) {
+              const fieldKey: keyof Fs = fieldKeys[i];
+              const constraintResultsForField = resultsArr[i].map((fn) => fn());
+              const unpackedConstraintMessages = await Promise.all(
+                constraintResultsForField
+              );
+              const message =
+                unpackedConstraintMessages.find(E.isLeft) || E.left(void null);
+              asyncResults[fieldKey] = pipe(
+                message,
+                E.swap,
+                E.getOrElse(passedField)
+              );
+            }
+            if (Object.values(asyncResults).some(Boolean)) {
+              throw asyncResults;
+            }
+          },
+          (err: ValidationResult<Fs>) => err
+        ),
+        TE.map(() => fields)
+      );
+    }
+
+    const hasFailed = flatten(resultsArr).some(E.isLeft);
 
     return hasFailed
       ? E.left(
           Object.keys(fields).reduce(
-            (acc: ValidationResult<Fs>, fieldKey: Fk, i) => ({
-              ...acc,
-              [fieldKey]: pipe(results[i], E.swap, E.getOrElse(passedField)),
-            }),
+            (acc: ValidationResult<Fs>, fieldKey: Fk) => {
+              const firstErr = results[fieldKey].find(E.isLeft) || E.right("");
+              return {
+                ...acc,
+                [fieldKey]: pipe(firstErr, E.swap, E.getOrElse(passedField)),
+              };
+            },
             fields
           )
         )
@@ -38,6 +90,15 @@ export function makeValidator<Fs extends Fields>(
 }
 
 const passedField = () => void null;
+
+// NB: this isn't a great way of figuring out whether the value is TaskEither or not...
+const isTaskEither = (value: any): boolean => typeof value === "function";
+
+const anyTaskEither = (values: any[]): boolean =>
+  Boolean(values.find(isTaskEither));
+
+const flatten = <T>(values: T[][]): T[] =>
+  values.reduce((acc, curr) => [...acc, ...curr], []);
 
 export type Fields = Record<string, any>;
 
@@ -48,7 +109,21 @@ export type ConstraintFn<Fv, Fk, Fs> = (
 ) => E.Either<string, Fv>;
 
 export type FieldConstraintsMap<Fs extends Fields> = {
-  [Fk in keyof Fs]: Array<ConstraintFn<Fs[Fk], Fk, Fs>>;
+  [Fk in keyof Fs]:
+    | ConstraintFn<Fs[Fk], Fk, Fs>
+    | Array<ConstraintFn<Fs[Fk], Fk, Fs>>;
 };
 
 export type ValidationResult<Fs extends Fields> = { [Fk in keyof Fs]: string };
+
+export type AsyncConstraintFn<Fv, Fk, Fs> = (
+  value: Fv,
+  key: Fk,
+  fields: Fs
+) => TE.TaskEither<string, Fv>;
+
+export type AsyncFieldConstraintsMap<Fs extends Fields> = {
+  [Fk in keyof Fs]:
+    | AsyncConstraintFn<Fs[Fk], Fk, Fs>
+    | Array<AsyncConstraintFn<Fs[Fk], Fk, Fs>>;
+};
--- src/test/fp.spec.ts
+++ src/test/fp.spec.ts
@@ -1,9 +1,10 @@
 import { pipe } from "fp-ts/lib/pipeable";
 import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
 import test from "tape";
 import { makeValidator } from "../fp";
 
-test("FP - Validates simple valid fields", (t) => {
+test("FP - Validates simple valid fields with Either", (t) => {
   t.plan(1);
   type Fields = {
     name: string;
@@ -33,7 +34,7 @@ test("FP - Validates simple valid fields", (t) => {
   );
 });
 
-test("FP - Validates simple invalid fields", (t) => {
+test("FP - Validates simple invalid fields Either", (t) => {
   t.plan(3);
   type Fields = {
     name: string;
@@ -69,6 +70,37 @@ test("FP - Validates simple invalid fields", (t) => {
   );
 });
 
+test("FP - Validates with TaskEither", async (t) => {
+  t.plan(3);
+  type Fields = {
+    name: string;
+    age: number;
+  };
+  const validate = makeValidator<Fields>({
+    name: asyncIsEqualTo("Bob"),
+    age: asyncIsGreaterThan(40),
+  });
+  const pipeline = pipe(
+    validate({ name: "Sam", age: 30 }),
+    TE.mapLeft((errors) => {
+      t.pass("Validation fails");
+      t.deepEqual(
+        errors,
+        {
+          name: "Expected Sam to equal Bob",
+          age: "Expected 30 to be greater than 40",
+        },
+        "Correct constraints returned"
+      );
+    }),
+    TE.map(() => {
+      t.fail("Expected validation to fail");
+    })
+  );
+  await pipeline();
+  t.pass("Pipeline finished");
+});
+
 const isOfType = (type: string) => <T>(value: T): E.Either<string, T> =>
   typeof value === type ? E.right(value) : E.left(`Expected a ${type}`);
 
@@ -89,3 +121,25 @@ const isGreaterThan = <T>(expected: T) => <V extends T>(
   value > expected
     ? E.right(value)
     : E.left(`Expected ${value} to be greater than ${expected}`);
+
+const asyncIsEqualTo = <T>(expected: T) => (value: T) =>
+  TE.tryCatch(
+    async () => {
+      if (value !== expected) {
+        throw new Error(`Expected ${value} to equal ${expected}`);
+      }
+      return value;
+    },
+    (err: Error) => err.message
+  );
+
+const asyncIsGreaterThan = (expected: number) => (value: number) =>
+  TE.tryCatch(
+    async () => {
+      if (value < expected) {
+        throw new Error(`Expected ${value} to be greater than ${expected}`);
+      }
+      return value;
+    },
+    (err: Error) => err.message
+  );
